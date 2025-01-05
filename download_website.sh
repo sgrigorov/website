e8061eb95afa36349d8b7948acf9a992e56929c0
Stan Grigorov <user.name@gmail.com>
2025-01-04 22:55:49 -0700

    My commit  message

--- download_website.sh
+++ download_website.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+# wget \
+#      --recursive \
+#      --no-clobber \
+#      --page-requisites \
+#      --adjust-extension \
+#      --span-hosts \
+#      --convert-links \
+#      --directory-prefix=dir1 \
+#      --domains squarespace.com,squarespace-cdn.com,naghedi-immigration.ca \
+#   https://www.naghedi-immigration.ca/
+
+# wget \
+#      --recursive \
+#      --no-clobber \
+#      --page-requisites \
+#      --adjust-extension \
+#      --span-hosts \
+#      --convert-links \
+#      --directory-prefix=dir2 \
+#      --domains naghedi-immigration.ca \
+#      --no-parent \
+#         www.naghedi-immigration.ca
+
 wget \
      --recursive \
      --no-clobber \
@@ -6,5 +29,19 @@ wget \
      --adjust-extension \
      --span-hosts \
      --convert-links \
+     --directory-prefix=dir3 \
+     --no-host-directories \
      --domains squarespace.com,squarespace-cdn.com,naghedi-immigration.ca \
-  https://www.naghedi-immigration.ca/
+  www.naghedi-immigration.ca
+
+wget \
+     --recursive \
+     --no-clobber \
+     --page-requisites \
+     --adjust-extension \
+     --span-hosts \
+     --convert-links \
+     --directory-prefix=dir3 \
+     --no-host-directories \
+     --domains squarespace.com,naghedi-immigration.ca \
+  www.naghedi-immigration.ca
