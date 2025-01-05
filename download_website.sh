91e150bf6aa7ff7b0d19bbb45759db7cd291db44
Stan Grigorov <user.name@gmail.com>
2025-01-04 23:42:24 -0700

    My commit  message

--- download_website.sh
+++ download_website.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# wget \
-#      --recursive \
-#      --no-clobber \
-#      --page-requisites \
-#      --adjust-extension \
-#      --span-hosts \
-#      --convert-links \
-#      --directory-prefix=dir1 \
-#      --domains squarespace.com,squarespace-cdn.com,naghedi-immigration.ca \
-#   https://www.naghedi-immigration.ca/
+wget \
+     --recursive \
+     --no-clobber \
+     --page-requisites \
+     --adjust-extension \
+     --span-hosts \
+     --convert-links \
+     --directory-prefix=dir1 \
+     --domains squarespace.com,squarespace-cdn.com,naghedi-immigration.ca \
+  https://www.naghedi-immigration.ca/
 
 # wget \
 #      --recursive \
