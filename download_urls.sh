a0a4ae835511312e740472c7ee1b8dd871daa243
Stan Grigorov <user.name@gmail.com>
2025-01-08 18:19:33 -0700

    My commit  message

--- download_urls.sh
+++ download_urls.sh
@@ -8,7 +8,4 @@ wget \
      --convert-links \
      --directory-prefix=dir1 \
      --domains squarespace.com,squarespace-cdn.com,naghedi-immigration.ca \
-  https://static1.squarespace.com/static/vta/5c5a519771c10ba3470d8101/scripts/floating-cart.11c2f1b3c1cb2ba0d418.js
-
-
-https://sgrigorov.github.io/website/dir1/static1.squarespace.com/static/vta/5c5a519771c10ba3470d8101/scripts/floating-cart.11c2f1b3c1cb2ba0d418.js
+  https://static1.squarespace.com/static/vta/5c5a519771c10ba3470d8101/scripts/$1
